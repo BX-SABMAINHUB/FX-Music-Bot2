83a4969b61f12bfd6e3ed71a28c247fd950269f9
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-20 19:50:32 +0100

    Update get-stats.js

--- api/get-stats.js
+++ api/get-stats.js
@@ -1,12 +1,17 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Alexgaming:Alex27Junio@cluster0.55a5siw.mongodb.net/?appName=Cluster0";
+
+// Tu link de conexión real
+const uri = "mongodb+srv://Alexgaming:Alex27Junio@cluster0.55a5siw.mongodb.net/?retryWrites=true&w=majority";
 
 export default async function handler(req, res) {
     const client = new MongoClient(uri);
     try {
         await client.connect();
         const db = client.db("flexus_data");
+        // Buscamos el registro que el bot actualiza
         const stats = await db.collection("ads_stats").findOne({ id: "global" });
+        
+        // Enviamos el número de vistas a la web
         res.status(200).json({ views: stats ? stats.views : 0 });
     } catch (e) {
         res.status(500).json({ error: e.message });
