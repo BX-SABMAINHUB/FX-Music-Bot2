227c20a254c6949b1753b32198ab0070e9804e94
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-23 20:57:38 +0100

    Update get-reviews.js

--- api/get-reviews.js
+++ api/get-reviews.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require('mongodb');
-
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
@@ -9,10 +8,9 @@ export default async function handler(req, res) {
         const db = client.db('flexus_data');
         const reviews = await db.collection('reviews')
             .find({})
-            .sort({ _id: -1 }) // Mostrar las más recientes primero
-            .limit(10)        // Mostrar solo las últimas 10
+            .sort({ timestamp: -1 }) // Usamos timestamp para ordenar
+            .limit(10)
             .toArray();
-
         res.status(200).json(reviews);
     } catch (e) {
         res.status(500).json({ error: e.message });
